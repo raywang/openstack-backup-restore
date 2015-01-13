3effd5922a2de80dce912b609620cbfefbf58da5
Ray Wang <wanglei1123@gmail.com>
2015-01-13 14:48:08 +0800

    finalize the backup

--- openstack-backup-restore.py
+++ openstack-backup-restore.py
@@ -26,7 +26,11 @@
 # 
 # Revovering Backup
 #       TODO
-#
+
+# usage:
+#   ./openstack-backup-restore.py backup --mysql -u root -pubuntu --dir test --keystone --nova --glance --cinder --neutron
+
+import pdb
 
 import os
 import sys
@@ -40,31 +44,30 @@ def parse_args():
     parser = argparse.ArgumentParser(description='Backup/Restore OpenStack \
             environment.')
 
-    parser.add_argument("action", metavar="backup | restore", choices=['backup', 'restore'], \
-            help="Backup OpenStack environment.")
+    parser.add_argument("action", metavar="backup | restore", 
+                        choices=['backup', 'restore'], 
+                        help="Backup OpenStack environment.")
 
     parser.add_argument("-u", "--db_user", help="Database User", default="root")
     parser.add_argument("-p", "--db_password", help="Database Password", 
                         default="")
+
     parser.add_argument("--db_host", help="Database Host", default="127.0.0.1")
     parser.add_argument("--dir", help="Backup Directory", default=".")
-    parser.add_argument("--mysql", help="Backup MySQL", 
-                        action="store_true")
-    parser.add_argument("--keystone", help="Backup Keystone", 
-                        action="store_true")
-    parser.add_argument("--nova", help="Backup Nova", 
-                        action="store_true")
-    parser.add_argument("--glance", help="Backup Glance", 
-                        action="store_true")
-    parser.add_argument("--cinder", help="Backup Cinder", 
-                        action="store_true")
+    parser.add_argument("--mysql", help="Backup MySQL", action="store_true")
+    parser.add_argument("--nova", help="Backup Nova", action="store_true")
+    parser.add_argument("--glance", help="Backup Glance", action="store_true")
+    parser.add_argument("--cinder", help="Backup Cinder", action="store_true")
+    parser.add_argument("--neutron", help="Backup Neutron", action="store_true")
+    parser.add_argument("--keystone", help="Backup Keystone", action="store_true")
     
-    args = parser.parse_args()
-
     return args
 
 
 def get_databases(args):
+
+    """get databases that need to be backed up"""
+
     list_db = "mysql --user={} --password={} --host={}  \
                 --skip-column-names --silent -e".format(
                 args.db_user, 
@@ -74,7 +77,6 @@ def get_databases(args):
     cmd = list_db.split()
     cmd.append('show databases')
     
-
     p = subprocess.Popen(cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE) 
     stdout, stderr = p.communicate()
     db_list = stdout.decode("utf-8").split()
@@ -134,21 +136,27 @@ def backup_db(args):
             ret = p.wait()
             if ret > 0:
                 print("ERROR: backup {} fail!".format(db))
+
     
-def backup_keystone(args):
-    keystone_files = '/etc/keystone'
-    
-    if not os.path.exists(keystone_files):
-        print("The {} is not exist.".format(keystone_files))
-        return
+def backup_openstack(args, service):
+
+    """Backup OpenStack Services"""
 
     if not os.path.exists(args.dir):
         os.mkdir(args.dir)
 
-    timestamp = datetime.now().strftime('%Y%m%d%H%M')
-    keystone_backup_dir = "{}/{}-{}".format(args.dir, "keystone", timestamp)
+    service_etc = "/etc/{}".format(service)
+    service_var = "/var/lib/{}".format(service)
+    service_files = [service_etc, service_var]
 
-    shutil.copytree(keystone_files, keystone_backup_dir)
+    timestamp = datetime.now().strftime('%Y%m%d%H%M')
+    service_backup_dir = "{}/{}-{}/".format(args.dir, service, timestamp)
+
+    for dir in service_files:
+        if not os.path.exists(dir):
+            print("The {} is not exist.".format(dir))
+
+        shutil.copytree(dir, "{}/{}".format(service_backup_dir, os.path.dirname(dir).replace('/', '')))
 
 
 def main():
@@ -161,15 +169,19 @@ def main():
             backup_db(args)
         if args.keystone:
             print("Keysotne will be backed up")
-            backup_keystone(args)
+            backup_openstack(args, "keystone")
         if args.nova:
             print("nova will be backed up")
+            backup_openstack(args, "nova")
         if args.glance:
             print("glance will be backed up")
+            backup_openstack(args, "glance")
         if args.cinder:
             print("cinder will be backed up")
-
-
+            backup_openstack(args, "cinder")
+        if args.neutron:
+            print("neutron will be backed up")
+            backup_openstack(args, "neutron")
 
 if __name__ == '__main__':
     sys.exit(main())
